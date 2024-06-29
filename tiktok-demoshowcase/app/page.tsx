efa31b63195ddd719dcb88b9f24e75794891788d
ryancollinsnc13 <ryancollinsnc13@gmail.com>
2024-06-28 19:33:40 -0700

    Initial commit

---
+++ tiktok-demoshowcase/app/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import Image from "next/image";
+import MainLayout from "./layouts/MainLayout"
+
+export default function Home() {
+  return (
+    <>
+      <MainLayout>
+        <div>
+          HOME
+        </div>
+      </MainLayout>
+    </>
+  );
+}
