966dabe34e5ab310d56f0b556da2306d38554cf9
ryancollinsnc13 <ryancollinsnc13@gmail.com>
2024-06-28 21:06:26 -0700

    Side nav bar complete

--- tiktok-demoshowcase/app/layouts/includes/SideNavMain.tsx
+++ tiktok-demoshowcase/app/layouts/includes/SideNavMain.tsx
@@ -36,6 +36,35 @@ export default function SideNavMain(){
                                 <MenuItemFollow user={{id:'1', name: "Test user", image: "https://placehold.co/50"}} /> 
                         </div>
                     </ClientOnly>
+
+                    <button className="lg:block hidden text-[#F02C56] pt-1.5 pl-2 text-[13px]">See all</button>
+
+                    {true ? (
+                        <div>
+                            <div className="border-b lg:ml-2 mt-2" />
+                            <h3 className="lg:block hidden text-xs text-gray-600 font-semibold pt-4 pb-2 px-2">Following accounts</h3>
+        
+                            <div className="lg:hidden block pt-3" />
+                            <ClientOnly>
+                                <div className="cursor-pointer">
+                                        <MenuItemFollow user={{id:'1', name: "Test user", image: "https://placehold.co/50"}} /> 
+                                </div>
+                            </ClientOnly>
+        
+                            <button className="lg:block hidden text-[#F02C56] pt-1.5 pl-2 text-[13px]">See more</button>
+                        </div>
+                    ) : null}
+
+                    <div className="lg:block hidden border-b lg:ml-2 mt-2" />
+
+                    <div className="lg:block hidden text-[11px] text-gray-500">
+                        <p className="pt-4 px-2">About Newsroom TikTok Shop Contact Careers ByteDance</p>
+                        <p className="pt-4 px-2">TikTok for Good Advertise Developers Transparency TikTok Rewards TikTok Browse TikTok Embeds</p>
+                        <p className="pt-4 px-2">Help Safety Terms Privacy Creator Portal Community Guidelines</p>
+                        <p className="pt-4 px-2">© 2023 TikTok</p>
+                    </div>
+
+                    <div className="pb-14"></div>
                 </div>
             </div>
         </>
