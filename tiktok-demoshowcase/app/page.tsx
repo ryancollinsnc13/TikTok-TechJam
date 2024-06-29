c3527e35675686c7418ea2272f24c23ce841f911
ryancollinsnc13 <ryancollinsnc13@gmail.com>
2024-06-28 21:23:16 -0700

    Home page base structure done

--- tiktok-demoshowcase/app/page.tsx
+++ tiktok-demoshowcase/app/page.tsx
@@ -2,13 +2,28 @@
 
 import Image from "next/image";
 import MainLayout from "./layouts/MainLayout"
+import ClientOnly from "./components/ClientOnly";
+import PostMain from "./components/PostMain";
 
 export default function Home() {
   return (
     <>
       <MainLayout>
-        <div>
-          HOME
+        <div className="mt-[80px]  w-[calc(100%-90px)] max-w-[690px] ml-auto">
+          <ClientOnly>
+              <PostMain post={{
+                id:'123',
+                user_id: '456',
+                video_url: '/dog.mp4',
+                text: 'temp text',
+                created_at: 'date',
+                profile: {
+                  user_id:'456',
+                  name:'Random user',
+                  image: 'https://placehold.co/100'
+                }
+              }} />
+          </ClientOnly>
         </div>
       </MainLayout>
     </>
